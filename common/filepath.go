8025b4e1bc64ab42997446fbce6da77bbb91d54a
Srlion <srlion32@gmail.com>
2025-11-28 00:32:13 +0200

    fix lsp not working in linux

--- common/filepath.go
+++ common/filepath.go
@@ -9,7 +9,8 @@ import (
 // FilePathClean is a combination of filepath.Clean and filepath.ToSlash
 //
 // Example:
-//   C:\H\ -> C:/H
+//
+//	C:\H\ -> C:/H
 func FilePathClean(p string) string {
 	// First do the normal OS-based cleanup
 	cleaned := filepath.Clean(p)
@@ -18,13 +19,12 @@ func FilePathClean(p string) string {
 }
 
 func FilePathToURI(path string) string {
-	uri := path
-	if runtime.GOOS == "windows" {
-		// Windows file URIs need three slashes: file:///C:/path
-		uri = "file:///" + url.PathEscape(uri)
-	} else {
-		// Unix-like systems: file:///path
-		uri = "file://" + url.PathEscape(uri)
+	u := &url.URL{
+		Scheme: "file",
+		Path:   path,
 	}
-	return uri
+	if runtime.GOOS == "windows" {
+		u.Path = "/" + path // Windows needs extra leading slash
+	}
+	return u.String()
 }
